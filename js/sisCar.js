556c7b780b7662a0bbf9e58616cb726e7be30236
Daniel Lima <fr.daniel.lima@gmail.com>
2017-11-07 15:33:43 -0300

    Aplicando ES6

--- js/sisCar.js
+++ js/sisCar.js
@@ -1,18 +1,18 @@
 (function( $, doc ){
   'use strict';
 
-  var app = (function appController(){
-    var $public = {};
+  const app = (function appController(){
+    let $public = {};
 
-    var $bodyTable = $('[data-js="bodyTable"]').get();
-    var urlAjax = 'http://localhost:3001/car';
+    const $bodyTable = $('[data-js="bodyTable"]').get();
+    const urlAjax = 'http://localhost:3001/car';
 
     function initEvents() {
       $('[data-js="form-register"]').on( 'submit', handleSubmit  );
     }
 
     function companyInfo() {
-      var ajax = new XMLHttpRequest();
+      let ajax = new XMLHttpRequest();
       ajax.open('GET', 'js/company.json', true);
       ajax.send();
       ajax.addEventListener('readystatechange', getCompanyInfo, false);
@@ -21,13 +21,13 @@
     function getCompanyInfo() {
       if ( !isReady.call( this ) )
         return;
-      var dadosCompany = JSON.parse( this.responseText );
+      const dadosCompany = JSON.parse( this.responseText );
       setCompanyInfo( dadosCompany );
     }
 
     function setCompanyInfo( dadosCompany ) {
-      var $companyName = $('[data-js="company-name"]').get();
-      var $companyPhone = $('[data-js="company-phone"]').get();
+      let $companyName = $('[data-js="company-name"]').get();
+      let $companyPhone = $('[data-js="company-phone"]').get();
       $companyName.appendChild( doc.createTextNode( dadosCompany.name ) );
       $companyPhone.appendChild( doc.createTextNode( dadosCompany.phone ) );
     }
@@ -38,39 +38,33 @@
 
     function handleSubmit(event) {
       event.preventDefault();
-      var car = {
+      const car = {
         image: $('[data-js="image"]').get().value,
         brandModel: $('[data-js="brand-model"]').get().value,
         plate: $('[data-js="plate"]').get().value,
         year:  $('[data-js="year"]').get().value,
         color: $('[data-js="color"]').get().value
       };
-      addCarServer( car );
+      addCar( car );
     }
 
-    function addCarServer( car ){
-      var ajax = new XMLHttpRequest();
+    function addCar( car ){
+      let ajax = new XMLHttpRequest();
       ajax.open( 'POST', urlAjax );
       ajax.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded' );
-      ajax.send( 
-        'image=' + car.image 
-        + '&brandModel=' + car.brandModel 
-        + '&year=' + car.year
-        + '&plate=' + car.plate
-        + '&color=' + car.color
-       );
+      ajax.send( `image=${car.image}&brandModel=${car.brandModel}&year=${car.year}&plate=${car.plate}&color=${car.color}` );
 
       ajax.onreadystatechange = function(){
         if( !isReady.call( this ) )
           return;
-        var data = JSON.parse( ajax.responseText );
+        const data = JSON.parse( ajax.responseText );
         if( data.message === 'success' )
           requestCars();
       };
     }
 
     function requestCars(){
-      var ajax = new XMLHttpRequest();
+      let ajax = new XMLHttpRequest();
       ajax.open( 'GET', urlAjax );
       ajax.send();
       ajax.addEventListener( 'readystatechange', hasCars, false );
@@ -79,21 +73,26 @@
     function hasCars( ){
       if( !isReady.call( this ) )
         return;
-      var cars = JSON.parse( this.responseText );
+      const cars = JSON.parse( this.responseText );
       atualizarBodyTable.apply( atualizarBodyTable, cars );
     };
 
     function atualizarBodyTable(){
-      var $trs = '';
-      Array.prototype.forEach.call( arguments, function( car ) {
-        $trs += '<tr>'
-                  + '<td><img src="' + car.image + '" class="img-thumbnail car_tamanho"></td>'
-                  + '<td>' + car.brandModel + '</td>'
-                  + '<td data-js="plate">' + car.plate + '</td>'
-                  + '<td>' + car.year + '</td>'
-                  + '<td>' + car.color + '</td>'
-                  + '<td><button data-js="remove" class="btn btn-danger"><i class="fa fa-remove"></i> Remover</Button></td>'
-                  + '</tr>';
+      let $trs = '';
+      Array.prototype.forEach.call( arguments, car => {
+        $trs += `
+          <tr>
+            <td><img src="${car.image}" class="img-thumbnail car_tamanho"></td>
+            <td>${car.brandModel}</td>
+            <td data-js="plate">${car.plate}</td>
+            <td>${car.year}</td>
+            <td>${car.color}</td>
+            <td>
+              <button data-js="remove" class="btn btn-danger">
+                <i class="fa fa-remove"></i> Remover
+              </button>
+            </td>
+        `; 
       });
       $bodyTable.innerHTML = $trs;
       initEventRemove();
@@ -105,25 +104,25 @@
 
     function handleClickRemove( event ){
       event.preventDefault();
-      var $trRemove = this.parentElement.parentElement;
-      removeCarSever( getPlate( $trRemove) );
-      $bodyTable.removeChild( $trRemove );
+      let $trRemove = this.parentElement.parentElement;
+      removeCar( $trRemove );
     }
 
-    function getPlate( $trCar ){
-      var plate;
-      Array.prototype.forEach.call( $trCar.children, function( element ){
+    function getPlate( $tdsCar ){
+      let plate;
+      Array.prototype.forEach.call( $tdsCar,  element => {
         if( element.getAttribute('data-js') === 'plate')
-           plate =  element.textContent;
+           plate = element.textContent;
       });
       return plate;
     }
 
-    function removeCarSever( plate ){s
-      var ajax = new XMLHttpRequest();
+    function removeCar( $trRemove ){
+      let ajax = new XMLHttpRequest();
       ajax.open( 'DELETE', urlAjax );
       ajax.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded' );
-      ajax.send( 'plate=' + plate );
+      ajax.send( `plate=${getPlate($trRemove.children)}` );
+      $bodyTable.removeChild( $trRemove );
     }
 
     $public.init = function init(){
